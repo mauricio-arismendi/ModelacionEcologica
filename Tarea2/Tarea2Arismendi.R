70e42432ce082db62025afb7729009cf90d147a5
Mauricio Arismendi <mauricio.arismendi.aedo@gmail.com>
2023-09-07 22:28:24 -0300

    Tarea2
    Inicio Tarea2

---
+++ Tarea2/Tarea2Arismendi.R
@@ -0,0 +1,13 @@
+# Tarea 2 Modelación Ecológica
+## Mauricio Arismendi Aedo
+### m.arismendi02@ufromail.cl
+#### Prof. Dr. Andrés Fuentes
+
+
+rm(list=ls())
+
+data <- read.csv(file = "C:\\Users\\mauri\\OneDrive\\Desktop\\richness_rncm2016.csv",
+                 header = TRUE,
+                 sep = ",",
+                 dec = ".",
+                 fill = TRUE)
