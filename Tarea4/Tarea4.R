38168db99f115d1e9a99db412f1d4fb2984931ab
Mauricio Arismendi <mauricio.arismendi.aedo@gmail.com>
2023-10-08 22:09:42 -0300

    Borrando datos de mas

---
+++ Tarea4/Tarea4.R
@@ -0,0 +1,11 @@
+#----------------------------------------
+##### T A R E A   4  ###################
+#---------------------------------------
+
+getwd()
+
+#Selecciona directorio de trabajo donde R lee archvos de entrada
+setwd("c:\\Users\\mauri\\OneDrive\\Documents\\GitHub\\ModelacionEcologica\\Tarea4") #windows
+
+
+list.files()
