ae612cea3f5455085ff328317f315746acc15aca
mauricio-arismendi <m.arismendi.aedo@gmail.com>
2023-11-17 13:25:11 -0300

    hola

--- Tarea7/Tarea7.R
+++ Tarea7/Tarea7.R
@@ -6,7 +6,33 @@
 ### m.arismendi02@ufromail.cl##
 ###############################
 
+
+
 rm(list = ls())
+
+#cargando datos y librerias
+
 library(vegan)
 data(dune)
 data(dune.env)
+dune.env
+install.packages("scatterplot3d", depedencies = TRUE)
+library(scatterplot3d)
+library(vegan)
+
+#conociendo datos
+
+summary(dune)
+summary(dune.env)
+str(dune)
+str(dune.env)
+
+head(dune)
+colnames(dune)
+str(dune)
+
+head(dune)
+tail(dune)
+
+#1
+
