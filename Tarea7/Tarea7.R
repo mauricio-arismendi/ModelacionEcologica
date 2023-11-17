955f520edcd75db94fed1fe008d311fd790c6fe2
mauricio-arismendi <m.arismendi.aedo@gmail.com>
2023-11-17 15:14:58 -0300

    Avance pregunta 1

--- Tarea7/Tarea7.R
+++ Tarea7/Tarea7.R
@@ -19,6 +19,7 @@ dune.env
 install.packages("scatterplot3d", depedencies = TRUE)
 library(scatterplot3d)
 library(vegan)
+library(permute)
 
 #conociendo datos
 
@@ -35,4 +36,8 @@ head(dune)
 tail(dune)
 
 #1
+#Profundidad horizonte A suelos respecto a especies y tipo de uso de suelos
+#dune.env$A1
+#plot(table(dune.env$A1, dune.env$Use), beside = TRUE, legend = TRUE, col = c("red", "green"), main = "Tamaño horizonte A por uso") 
 
+plot(dune.env$Use, dune.env$A1, main = "Profundidad Horizonte A, respecto a Uso de Suelo", xlab = "Uso de Suelo", ylab = "Profundidad de Horizonte A")
