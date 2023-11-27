e390f8b55102ca12bfe659d87640807f4ed52c2a
Mauricio Arismendi <mauricio.arismendi.aedo@gmail.com>
2023-11-27 01:58:38 -0300

    casi terminada parte 1

--- Tarea8/Tarea8.R
+++ Tarea8/Tarea8.R
@@ -14,4 +14,16 @@ library(spatstat)
 aleatorio <- rpoispp(lambda = 4, win = owin(c(0,14), c(0,20)))
 aleatorio
 
-plot(aleatorio)
+
+plot(aleatorio, pch=19, cex=0.5, main="Patrón Aleatorio")
+summary(aleatorio)
+?pch
+
+
+
+env.rand<-envelope(aleatorio, pcf, nsim=99, correction="isotropic",
+                   r=seq(0, 7, by=0.1))
+?envelope
+
+plot(env.rand, ylim=c(0.5,2), xlim=c(0,4), las=1, main="PCF patron aleatorio",
+     legend=TRUE)
