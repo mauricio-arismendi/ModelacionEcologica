3d9ae26384ffd8057eef01e60bcb8d5a1f9f2b58
Mauricio Arismendi <mauricio.arismendi.aedo@gmail.com>
2023-11-30 00:59:57 -0300

    Tarea 8 lista

--- Tarea8/Tarea8.R
+++ Tarea8/Tarea8.R
@@ -6,7 +6,7 @@
 ### m.arismendi02@ufromail.cl######
 ###################################
 # Ingeniería en Recursos Naturales#
-
+??colsum
 rm(list = ls())
 
 #1
@@ -51,5 +51,42 @@ plot(env.agg, ylim=c(0.5,2), xlim=c(0,4), las=1,
 summary(agregado)
 
 #3
+list.files()
+chinamuerta <- read.csv("veg_chinamuerta.csv")
+str(chinamuerta)
+lim <- owin(c(0,50),c(0,50))
+plot(lim)
+
+#subseteamos, porque con factor se me alargó demasiado
+
+altaSeveridad <- subset(chinamuerta, severidad =="alta")
+altaSeveridadppp <- ppp(altaSeveridad$x, altaSeveridad$y, window=lim)
+plot(altaSeveridadppp, cex=1, pch=21, bg="red", main="Rebrotes en zonas de alta Severidad")
+
+as.pcf <- pcf(altaSeveridadppp, r=seq(0,5, by=0.1))
+plot(as.pcf, xlim=c(0,5), main=("PCF Rebrotes en Zonas de Alta Severidad"), xlab= c
+     ("Distancia radial"))
+ms.env<-envelope(altaSeveridadppp,pcf, correction="better", nsim=99,
+                 r=seq(0,5, by=0.1))
+plot(ms.env,ylim=c(0.3,3.8),main="PCF Rebrotes en Alta Severidad",
+     xlim=c(0,5), xlab="Distancia Radial. (r)", las=1)  
+
+#Mediana severidad subset y puntos
+mediaSeveridad<-subset(chinamuerta, severidad=="media")
+ms.ppp<-ppp(mediaSeveridad$x, mediaSeveridad$y, window=lim)
+summary(ms.ppp)
+plot(ms.ppp, cex=1, pch=21, bg="yellow", main="Rebrotes en zonas de media Severidad")
+
+
+
+#media severidad
+ms.pcf<-pcf(ms.ppp, r=seq(0,5, by=0.1))
+plot(ms.pcf, xlim=c(0,5), xlab = ("Distancia Radial"))
+     
+#intervalo de confianza 
+ms.env<-envelope(ms.ppp,pcf, correction="isotropic", nsim=99,
+                      r=seq(0,5, by=0.1))
+     plot(ms.env,ylim=c(0.1,100),main="PCF Rebrotes en Alta Severidad",
+          xlim=c(0,5), xlab="Distancia Radial. (r)", las=1)     
 
 
